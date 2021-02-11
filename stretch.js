3ebcf21242350c0f47c13e441764522b3fb907f3
Greg Brewton <gbrewton1980@gmail.com>
2021-02-10 21:00:59 -0700

    Initial branch commit

---
+++ stretch.js
@@ -0,0 +1,22 @@
+/**
+ * 1. Write a function that would allow you to do this using a closure. (This is another interview question we've seen before - when you're ready for answers, view an explanation [here](https://www.coderbyte.com/algorithm/3-common-javascript-closure-questions)).
+
+```js
+var addSix = createBase(6);
+addSix(10); // returns 16
+addSix(21); // returns 27
+```
+
+2. Research the differences between functional programming and object oriented programming. Then, describe the pros and cons of functional programming vs object-oriented programming. This is a common interview question and great practice!
+
+## Resources
+
+📚 [Scope and Closures Guide](https://css-tricks.com/javascript-scope-closures/)
+
+🧠 ["I never Understood Closures" Blog](https://medium.com/dailyjs/i-never-understood-javascript-closures-9663703368e8)
+
+## Submission Format
+
+Please see Canvas for cohort specific submission instructions 
+
+ */
